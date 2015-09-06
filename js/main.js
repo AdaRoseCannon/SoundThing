35c39a62856f3fa449cb57ce6e5622db59876135
Ada Rose Edwards <ada.edwards@ft.com>
2015-09-06 23:32:22 +0100

    Use a vertex shader rather than a worker

--- js/main.js
+++ js/main.js
@@ -6,26 +6,21 @@
 /**
  * Local Vars
  */
-var began = false;
 var analyser; // Audio analyser object
 var maxBins = 32; // Reduce freq resolution
 var cutOff = 0.5; // Only draw the bottom half of the spectrum.
 var mediaStreamSource; // The audio input object
 
-var container;
+var container, mesh;
 var camera, scene, renderer, renderMethod, stereoEffect;
-var mesh, geometry;
 
 var xRotOffset = 0;
 var yRotOffset = 0;
 
-var tempScale = 1;
-var meshScale = tempScale;
-
 var WIDTH = document.documentElement.clientWidth;
 var HEIGHT = WIDTH * document.documentElement.clientHeight / document.documentElement.clientWidth;
 
-var worker = new Worker('js/processVerts.js');
+var audioBins = Array.apply(null, new Array(maxBins)).map(function() { return 0.01 });
 
 /**
  * Normalise Features
@@ -61,31 +56,14 @@ function initThreeJS() {
 	return addScript('js/three.min.js').then(function () {
 		container = document.body;
 
-		camera = new THREE.PerspectiveCamera(90, WIDTH / HEIGHT, 1, 2000);
-		camera.position.z = 1300;
+		camera = new THREE.PerspectiveCamera(90, WIDTH / HEIGHT, 0.01, 20);
+		camera.position.z = 2;
 
 		scene = new THREE.Scene();
 
-		var lightGreen = new THREE.DirectionalLight(0xffff00, 0.5);
-		lightGreen.position.set(-100, -100, 0);
-		scene.add(lightGreen);
-
-		var lightRed = new THREE.DirectionalLight(0xff00ff, 0.5);
-		lightRed.position.set(100, 0, 0);
-		scene.add(lightRed);
-
-		var lightCyan = new THREE.DirectionalLight(0x00ffff, 0.5);
-		lightCyan.position.set(0, 100, 100);
-		scene.add(lightCyan);
-
 		var light2 = new THREE.DirectionalLight(0xffffff, 0.5);
-		light2.position.set(0, 2000, 0);
-		light2.castShadow = true;
-		light2.shadowDarkness = 0.5;
-		light2.shadowCameraTop = 800;
-		light2.shadowCameraBottom = -800;
-		light2.shadowCameraLeft = -800;
-		light2.shadowCameraRight = 800;
+		light2.position.set(0, 8, 0);
+		light2.lookAt(new THREE.Vector3(0, 0, 0));
 		scene.add(light2);
 
 		var loader = new THREE.JSONLoader();
@@ -95,7 +73,6 @@ function initThreeJS() {
 		});
 
 		renderer.setSize(WIDTH, HEIGHT);
-		renderer.shadowMapEnabled = true;
 		container.appendChild(renderer.domElement);
 		renderer.domElement.style.width = '100%';
 		renderer.domElement.style.height = '100%';
@@ -103,44 +80,65 @@ function initThreeJS() {
 		renderMethod = renderer;
 		window.addEventListener('resize', resizeToWindow, false);
 
-		var floorMaterial = new THREE.MeshLambertMaterial({
-			color: 0xffffff,
-			specular: 0xffffff,
-			shininess: 20,
-			shading: THREE.FlatShading
-		});
-
-		var floorGeometry = new THREE.PlaneGeometry(10000, 10000);
-
-		var floorMesh = new THREE.Mesh(floorGeometry, floorMaterial);
-		floorMesh.rotation.x = -Math.PI / 2;
-		floorMesh.position.y = -500;
-		floorMesh.position.z = -500;
-		scene.add(floorMesh);
-
 		return new Promise(function (resolve) {
-			loader.load("js/bunny.js", function(geom) {
-				geometry = geom;
-				worker.postMessage({
-					cmd: 'updateGeom',
-					vertices: geometry.vertices
+			loader.load("js/bunny.json", function(geom) {
+
+				var path = "images/";
+				var format = '.jpg';
+				var urls = [
+					path + 'px' + format, path + 'nx' + format,
+					path + 'py' + format, path + 'ny' + format,
+					path + 'pz' + format, path + 'nz' + format
+				];
+				var reflectionCube = THREE.ImageUtils.loadTextureCube(urls);
+				reflectionCube.format = THREE.RGBFormat;
+				var shiny = new THREE.MeshLambertMaterial({ 
+					color: 0xaa0000,
+					specular: 0x440000,
+					envMap: reflectionCube,
+					combine: THREE.MixOperation,
+					reflectivity: 0.3,
+					metal: true
+				});
+				window.shiny = shiny;
+
+				var mat = new THREE.ShaderMaterial({
+					uniforms: {
+						audioBins: {
+							type: "fv1",
+							value: audioBins
+						}
+					},
+					attributes: {},
+					vertexShader: document.getElementById('vertShader').text,
+					fragmentShader: document.getElementById('fragShader').text
 				});
 				
-				mesh = new THREE.Mesh(geom, new THREE.MeshLambertMaterial({
-					color: 0xffffff,
-					specular: 0xffffff,
-					shininess: 20,
-					shading: THREE.FlatShading
-				}));
-				mesh.scale.set(100, 100, 100);
+				mesh = new THREE.Mesh(geom, mat);
 				scene.add(mesh);
-				mesh.castShadow = true;
-				floorMesh.receiveShadow = true;
-				light2.lookAt(mesh);
 				resolve();
 			});
 		});
 	});
+}
+
+var renderingStarted = false;
+function render(rm) {
+	if (rm) renderMethod = rm;
+	if (!renderingStarted) {
+		renderLoop();
+		renderingStarted = true;
+	}
+}
+
+function renderLoop() {
+	if (!mesh) return;
+	var time = Date.now() * 0.001;
+	getAudioData();
+	mesh.rotation.x = time * 0.25 + xRotOffset;
+	mesh.rotation.y = time * 0.5 + yRotOffset;
+	renderMethod.render(scene, camera);
+	requestAnimationFrame(renderLoop);
 }
 
 function resizeToWindow() {
@@ -165,12 +163,12 @@ function initTouch() {
 			xRotOffset -= ev.velocityY / 10;
 			yRotOffset -= ev.velocityX / 10;
 		});
-		hammertime.on('pinchstart', function() {
-			tempScale = meshScale;
-		});
-		hammertime.on('pinchmove', function(ev) {
-			meshScale = tempScale * ev.scale;
-		});
+		// hammertime.on('pinchstart', function() {
+		// 	tempScale = meshScale;
+		// });
+		// hammertime.on('pinchmove', function(ev) {
+		// 	meshScale = tempScale * ev.scale;
+		// });
 	});
 }
 
@@ -262,8 +260,6 @@ function init() {
 							console.log (e.stack);
 						});
 				}
-			}).then (function () {
-				beginBunny();
 			});
 		});
 	}
@@ -280,74 +276,20 @@ function init() {
 		// Create an AudioNode from the stream.
 		mediaStreamSource = audioContext.createMediaStreamSource(stream);
 		mediaStreamSource.connect(analyser);
-		worker.postMessage({
-			cmd: 'init',
-			frequencyBinCount: Math.floor(analyser.frequencyBinCount * cutOff) - 1
-		});
 
 	}, function(err) {
 		console.log(err);
 	});
 }
 
-function beginBunny() {
-	if (began) return;
-	began = true;
-
-	(function geomLoop() {
-		requestAnimationFrame(function() {
-			getAudioData();
-			updateGeom();
-			geomLoop();
-		});
-	})();
-}
-
 function getAudioData() {
-	if (!analyser) return;
+	if (!analyser) return audioBins;
 	var freqData = new Float32Array(analyser.frequencyBinCount);
 	var min = analyser.minDecibels;
 	var max = analyser.maxDecibels;
 	analyser.getFloatFrequencyData(freqData);
-	var out = [];
 	for (var i = 1; i < freqData.length * cutOff; i++) {
-		out.push(50 * (freqData[i] - min) / (max - min));
+		audioBins[i] = (50 * (freqData[i] - min) / (max - min));
 	}
-	worker.postMessage({
-		cmd: 'updateAudioData',
-		audioData: out
-	});
-
-}
-worker.addEventListener('message', function (e) {
-	var data = e.data;
-	switch (data.cmd) {
-		case 'newVertices':
-			geometry.vertices = data.vertices;
-			geometry.dynamic = true;
-			geometry.verticesNeedUpdate = true;
-			break;
-		case 'log':
-			console.log(e.data);
-			break;
-	}
-});
-
-function updateGeom() {
-	worker.postMessage({cmd: 'fetchVertices'});
-}
-
-var renderingStarted = false;
-function render(rm) {
-	if (rm) renderMethod = rm;
-	if (renderingStarted) return;
-	requestAnimationFrame(function() {
-		if (!mesh) return;
-		var time = Date.now() * 0.001;
-		mesh.scale.set(100 * meshScale, 100 * meshScale, 100 * meshScale);
-		mesh.rotation.x = time * 0.25 + xRotOffset;
-		mesh.rotation.y = time * 0.5 + yRotOffset;
-		renderMethod.render(scene, camera);
-		render();
-	});
+	return audioBins;
 }
